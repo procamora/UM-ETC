d0c0711efd9402015534addec9fa6bfc4281ea4f
Alumno <alumno@UbuntuEva>
2016-03-04 12:49:26 +0100

    prac2 en proceso

--- boletin/prac2/integer_to_string_incompleto.s
+++ boletin/prac2/integer_to_string_incompleto.s
@@ -1,13 +1,13 @@
         .data
 buffer:	.space 256
-str000:	.asciiz	"Introduzca un número (n) en base 10: "
+str000:	.asciiz	"Introduzca un nï¿½mero (n) en base 10: "
 str001:	.asciiz	": "
 str002:	.asciiz	"n en base "
 str003:	.asciiz	"\n"
         
         .text
-# mueve la línea siguiente justo antes de la versión que desees probar
-integer_to_string:
+# mueve la lï¿½nea siguiente justo antes de la versiï¿½n que desees probar
+
 integer_to_string_v0:			# ($a0, $a1, $a2) = (n, base, buf)
         move    $t0, $a2		# char *p = buff
 	# for (int i = n; i > 0; i = i / base) {
@@ -24,10 +24,40 @@ B0_3:   blez	$t1, B0_7		# si i <= 0 salta el bucle
 B0_7:	sb	$zero, 0($t0)		# *p = '\0'
 B0_10:	jr	$ra
 
+integer_to_string:
 integer_to_string_v1:           	# ($a0, $a1, $a2) = (n, base, buf)
-	# TODO
-	break
-	jr	$ra
+        move 	$t0, $a2		# char *p = buff
+	# for (int i = n; i > 0; i = i / base) {
+        move	$t1, $a0		# int i = n
+        
+B1_3:  
+	blez	$t1, B1_7		# si i <= 0 salta el bucle
+	div		$t1, $a1		# i / base
+	mflo		$t1			# i = i / base
+	mfhi		$t2			# d = i % base
+	addiu	$t2, $t2, '0'		# d + '0'
+	sb 		$t2, 0($t0)		# *p = $t2 
+	addiu	$t0, $t0, 1		# ++p
+	j		B1_3			# sigue el bucle
+        # }
+
+B1_7:	
+	bgt 		$t3, $t0, B1_10
+	
+	sub		$t0, $t0, 1
+	move	$t3, $a2
+	lb		$t4, 0($t3)
+	lb		$t5, 0($t0)
+	sb		$t5, $t3
+	sb		$t4, $t0
+	add		$t3, $t3, 1
+	div		$t0 $t0, 1
+	j		B1_7
+	
+	sb		$zero, 0($t0)		# *p = '\0'
+	
+B1_10:	jr	$ra
+
 
 integer_to_string_v2:           	# ($a0, $a1, $a2) = (n, base, buf)
 	# TODO
@@ -44,7 +74,7 @@ integer_to_string_v4:			# ($a0, $a1, $a2) = (n, base, buf)
         break
         jr	$ra
 
-# Imprime el número recibido en base 10 seguido de un salto de linea
+# Imprime el nï¿½mero recibido en base 10 seguido de un salto de linea
 test1:					# $a0 = n
 	addiu	$sp, $sp, -4
 	sw	$ra, 0($sp)
@@ -59,7 +89,7 @@ test1:					# $a0 = n
 	addiu	$sp, $sp, 4
 	jr	$ra
 
-# Imprime el número recibido en todas las bases entre 2 y 36
+# Imprime el nï¿½mero recibido en todas las bases entre 2 y 36
 test2:					# $a0 = n
 	addiu	$sp, $sp, -12
 	sw	$ra, 8($sp)
@@ -87,7 +117,7 @@ B6_1:	la	$a0, str002
 	la	$a0, str003
 	jal	print_string		# print_string("\n")
 	addiu	$s1, $s1, 1		# ++b
-        li	$t0, 36
+        li	$t0, 6
 	ble	$s1, $t0, B6_1		# sigue el bucle si b <= 36
 	# } 
 	lw	$s0, 0($sp)
@@ -102,7 +132,7 @@ main:
 	sw	$ra, 4($sp)
 	sw	$s0, 0($sp)
 	la	$a0, str000
-	jal	print_string		# print_string("Introduzca un número (n) en base 10: ")
+	jal	print_string		# print_string("Introduzca un nï¿½mero (n) en base 10: ")
 	jal	read_integer
 	move	$s0, $v0		# int n = read_integer()
 	move	$a0, $s0
